285dcda2852b9cc9d11754d6b93ce9be01f275bc
daqi.jia <744342065@qq.com>
2020-07-01 11:25:01 +0800

    my study with C#

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 10;
+            int num2 = 20;
+            for (int i=0;i<10;i++)
+            {
+                Console.WriteLine("num1 = {0} ,num2 = {1}",num1,num2);
+                changeNum(ref num1,ref num2);
+                Console.WriteLine("num1 = {0} ,num2 = {1}", num1, num2);
+                num1++;
+                num2++;
+            }
+
+            Console.WriteLine("Hello World!");
+            Object obj = Console.ReadKey();
+            Console.WriteLine(obj.ToString());
+        }
+
+        public static void changeNum(ref int x,ref int y)
+        {
+            int tmp = x;
+            x = y;
+            y = tmp;
+        }
+    }
+}
